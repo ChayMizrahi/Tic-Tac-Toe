00bf5b88dfee70cd50fb55eb016599e3b07a8f6b
ChayMizrahi <31chay@gmail.com>
2020-08-05 12:15:01 +0300

    Puts all global variables into closures

--- script.js
+++ script.js
@@ -1,49 +1,94 @@
-const allCells = document.getElementsByTagName('td');
-const buttonClear = document.getElementById('button_clear');
-const userTurn = document.querySelector('.user-turn');
-const spinner = document.querySelector('.spinner');
+function ticTakToeGame() {
 
-for (const cell of allCells) {
-    cell.addEventListener('click', function(){
-        if(!cell.classList.contains('ocupied')){
-            cell.innerHTML = 'X';
-            cell.classList.add('ocupied');
-            if (!isGameOver()) {
-                userTurn.style.display = "none";
-                spinner.style.display = "block";
-                setTimeout(() => {
-                    computerTurn();
-                }, 1500);
+    const g_allCells = document.getElementsByTagName('td');
+    const g_buttonClear = document.getElementById('button_clear');
+    const g_userTurn = document.querySelector('.user-turn');
+    const g_spinner = document.querySelector('.spinner');
+
+    /**
+     * Adds the X add the class 'ocupied' to the cilcked cell.
+     * @param {*} event 
+     */
+    function onClickCell(event) {
+        const cell = event.target;
+        const isOcupied = cell.classList.contains('ocupied');
+        
+        if (isOcupied)
+            return;
+       
+        cell.innerHTML = 'X';
+        cell.classList.add('ocupied');
+
+        if (isGameOver())
+            return;
+
+        toggleUserAndComputerTurn();
+        setTimeout(() => {computerTurn()},1500);
+    }
+
+    /**
+     * Removes the class ocupied and the innerHTML from all cells.
+     */
+    function onClickCkearButton() {
+        for (const cell of g_allCells) {
+            cell.innerHTML = '';
+            cell.classList.remove('ocupied');
+        }
+    }
+
+    /**
+     * Replaces the text shows whose turn.
+     */
+    function toggleUserAndComputerTurn() {
+        g_userTurn.style.display = g_userTurn.style.display === 'none' ? 'block' : 'none';
+        g_spinner.style.display = g_spinner.style.display === 'block' ? 'none' : 'block';
+    }
+
+    /**
+     * Checks is the game is over.
+     */
+    function isGameOver() {
+        for (const cell of g_allCells) {
+            if (cell.innerHTML === '') {
+                return false;
             }
         }
-    })
-}
-
-buttonClear.addEventListener('click', function(){
-    for (const cell of allCells) {
-        cell.innerHTML = '';
-        cell.classList.remove('ocupied');
+        return true;
     }
-})
 
-
-function computerTurn() {
-    for (const cell of allCells) {
-        if (cell.innerHTML === '') {
-            cell.innerHTML = 'O';
-            cell.classList.add('ocupied');
-            spinner.style.display = "none";
-            userTurn.style.display = "block";
-            return;
+    /**
+     * Make the computer turn.
+     */
+    function computerTurn() {
+        for (const cell of g_allCells) {
+            if (cell.innerHTML === '') {
+                cell.innerHTML = 'O';
+                cell.classList.add('ocupied');
+                toggleUserAndComputerTurn();
+                return;
+            }
         }
     }
+
+    /**
+     * Init all the events.
+     */
+    function initEvents() {
+        for (const cell of g_allCells) {
+            cell.addEventListener('click', onClickCell);
+        }
+        g_buttonClear.addEventListener('click', onClickCkearButton);
+    }
+
+    /**
+     * Init the game.
+     */
+    this.init = function () {
+        initEvents();
+    }
+
 }
 
-function isGameOver() {
-    for (const cell of allCells) {
-        if (cell.innerHTML === '') {
-            return false;
-        }
-    }
-    return true;
-}
+const objGame = new ticTakToeGame();
+objGame.init();
+
